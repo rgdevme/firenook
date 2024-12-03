7172ec4cecfb65499bbadf98080907e9d02f914a
Rafael González <reggon81@gmail.com>
2024-12-03 20:59:13 +0100

    fix: change to subscription and fix types

--- src/context/collection.tsx
+++ src/context/collection.tsx
@@ -10,6 +10,8 @@ import {
 import { fireborm } from '../firebase'
 import { useCollectionsList } from './collectionsList'
 import { useParamsContext } from './params'
+import { getCountFromServer, query, Timestamp } from 'firebase/firestore'
+import { PropertyType } from '../firebase/types/Property'
 
 type FirebormStore = ReturnType<ReturnType<typeof FireBorm>['initializeStore']>
 
@@ -17,8 +19,8 @@ const CollectionCtx = createContext({
 	store: null as null | FirebormStore,
 	currentDoc: null as null | ReturnType<FirebormStore['docRef']>,
 	count: 0,
-	selection: new Set() as Set<string | number>,
-	setSelection: (() => {}) as Dispatch<Set<string | number>>
+	selection: new Set() as Set<string>,
+	setSelection: (() => {}) as Dispatch<Set<string>>
 })
 
 export const CollectionProvider = ({ children }: PropsWithChildren) => {
@@ -28,8 +30,8 @@ export const CollectionProvider = ({ children }: PropsWithChildren) => {
 	const [currentDoc, setCurrentDoc] = useState<null | ReturnType<
 		FirebormStore['docRef']
 	>>(null)
-	const [count /*, setCount */] = useState(0)
-	const [selection, setSelection] = useState<Set<string | number>>(new Set())
+	const [count, setCount] = useState(0)
+	const [selection, setSelection] = useState(new Set<string>())
 
 	useEffect(() => {
 		if (!current) return
@@ -38,15 +40,37 @@ export const CollectionProvider = ({ children }: PropsWithChildren) => {
 			toDocument: ({ _ref, id, ...doc }) => doc,
 			toModel: doc => {
 				const { id, ref } = doc
-				// const data = doc.data()
-				return { id, _ref: ref }
+				const data = {} as any
+
+				if (current.schema.some(x => x.show)) {
+					const docData = doc.data()
+					for (const key in docData) {
+						const type = current.schema.find(s => s.key === key)!.type
+
+						if (type === PropertyType.timestamp) {
+							data[key] = (docData[key] as Timestamp).toDate()
+						} else {
+							data[key] = docData[key]
+						}
+					}
+				}
+				return { id, _ref: ref, ...data }
 			}
 		})
 		setStore(newStore)
-		// newStore.count(...[]).then(c => {
-		// 	console.log({ c })
-		// 	setCount(c)
-		// })
+
+		const q = query(newStore.ref, ...[])
+		getCountFromServer(q).then(res => {
+			const count = res.data().count
+			setCount(count)
+		})
+
+		// newStore
+		// 	.count()
+		// 	.then(c => {
+		// 		console.log({ c })
+		// 	})
+		// 	.catch(console.error)
 	}, [current])
 
 	useEffect(() => {
