ae45dc2781b3b475f87f80460a0cb257d5048d44
Rafael González <reggon81@gmail.com>
2024-12-07 02:00:35 +0100

    feat: add firebase emulator to test storage

--- index.tsx
+++ index.tsx
@@ -1,14 +1,17 @@
 import { initFB } from './src/firebase'
 
-initFB({
-	apiKey: 'AIzaSyCHJdqYGjsXWlciLt21lN5i8qheX6tBqoQ',
-	authDomain: 'rgdevme.firebaseapp.com',
-	projectId: 'rgdevme',
-	storageBucket: 'rgdevme.firebasestorage.app',
-	messagingSenderId: '615649263196',
-	appId: '1:615649263196:web:26c5e77cf3c4f56de1f099',
-	measurementId: 'G-7P0LGV13FP'
-}).then(async app => {
+initFB(
+	{
+		apiKey: 'AIzaSyCHJdqYGjsXWlciLt21lN5i8qheX6tBqoQ',
+		authDomain: 'rgdevme.firebaseapp.com',
+		projectId: 'rgdevme',
+		storageBucket: 'rgdevme.firebasestorage.app',
+		messagingSenderId: '615649263196',
+		appId: '1:615649263196:web:26c5e77cf3c4f56de1f099',
+		measurementId: 'G-7P0LGV13FP'
+	},
+	true
+).then(async app => {
 	const { createRoot } = await import('react-dom/client')
 	const { Firenook } = await import('./src/index')
 
