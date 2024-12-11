74730b53bdc3aee25102b69e6e02c74bf6ee199e
Rafael González <reggon81@gmail.com>
2024-12-11 22:21:58 +0100

    feat: add login func

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,39 +2,86 @@ import {
 	Button,
 	Card,
 	CardBody,
-	Input,
-	Link,
-	Tab,
-	Tabs
+	Image
+	// Input,
+	// Link,
+	// Tab,
+	// Tabs
 } from '@nextui-org/react'
-import { useState } from 'react'
-
+import { useToggle } from '@uidotdev/usehooks'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+// import { useState } from 'react'
+// import { useForm } from 'react-hook-form'
+import { FcGoogle } from 'react-icons/fc'
 export const Login = () => {
-	const [selected, setSelected] = useState('login')
+	const [loading, setLoading] = useToggle()
+	// const [selected, setSelected] = useState('login')
+	const auth = getAuth()
+	// const { handleSubmit, register } = useForm({
+	// 	disabled: loading,
+	// 	defaultValues: {
+	// 		email: '',
+	// 		password: '',
+	// 		name: '' as string | undefined
+	// 	}
+	// })
+
+	const onSubmit = async data => {
+		try {
+			setLoading(true)
+			const provider = new GoogleAuthProvider()
+			const result = await signInWithPopup(auth, provider)
+			// This gives you a Google Access Token. You can use it to access the Google API.
+			const credential = GoogleAuthProvider.credentialFromResult(result)
+			const token = credential?.accessToken
+			// The signed-in user info.
+			const user = result.user
+			// IdP data available using getAdditionalUserInfo(result)
+			// ...
+			console.log({ token, user })
+		} catch (error) {
+			console.error({ error })
+		} finally {
+			setLoading(false)
+		}
+	}
 
 	return (
-		<div className='flex flex-col w-full max-w-lg'>
-			<Card className='max-w-full w-[340px] h-[400px]'>
-				<CardBody className='overflow-hidden'>
-					<Tabs
+		<div className='flex flex-col w-full h-full justify-center items-center'>
+			<Card className='max-w-[340px]'>
+				<CardBody className='overflow-hidden flex flex-col gap-4'>
+					<Image src='https://placehold.co/600x400/EEE/31343C' />
+					<Button
+						startContent={<FcGoogle size={24} />}
+						variant='light'
+						color='primary'
+						onClick={onSubmit}
+						isLoading={loading}>
+						Sign in with Google
+					</Button>
+					{/* <Tabs
 						fullWidth
 						size='md'
 						aria-label='Tabs form'
 						selectedKey={selected}
 						onSelectionChange={k => setSelected(k as string)}>
 						<Tab key='login' title='Login'>
-							<form className='flex flex-col gap-4'>
+							<form
+								className='flex flex-col gap-4'
+								onSubmit={handleSubmit(onSubmit)}>
 								<Input
 									isRequired
 									label='Email'
 									placeholder='Enter your email'
 									type='email'
+									{...register('email', { required: true })}
 								/>
 								<Input
 									isRequired
 									label='Password'
 									placeholder='Enter your password'
 									type='password'
+									{...register('password', { required: true })}
 								/>
 								<p className='text-center text-small cursor-pointer'>
 									Need to create an account?{' '}
@@ -50,24 +97,28 @@ export const Login = () => {
 							</form>
 						</Tab>
 						<Tab key='sign-up' title='Sign up'>
-							<form className='flex flex-col gap-4 h-[300px]'>
+							<form
+								className='flex flex-col gap-4 h-[300px]'
+								onSubmit={handleSubmit(onSubmit)}>
 								<Input
 									isRequired
 									label='Name'
 									placeholder='Enter your name'
-									type='password'
+									{...register('name', { required: true })}
 								/>
 								<Input
 									isRequired
 									label='Email'
 									placeholder='Enter your email'
 									type='email'
+									{...register('email', { required: true })}
 								/>
 								<Input
 									isRequired
 									label='Password'
 									placeholder='Enter your password'
 									type='password'
+									{...register('password', { required: true })}
 								/>
 								<p className='text-center text-small cursor-pointer'>
 									Already have an account?{' '}
@@ -76,13 +127,13 @@ export const Login = () => {
 									</Link>
 								</p>
 								<div className='flex gap-2 justify-end'>
-									<Button fullWidth color='primary'>
+									<Button type='submit' fullWidth color='primary'>
 										Sign up
 									</Button>
 								</div>
 							</form>
 						</Tab>
-					</Tabs>
+					</Tabs> */}
 				</CardBody>
 			</Card>
 		</div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { NextUIProvider } from '@nextui-org/react'
+import { useToggle } from '@uidotdev/usehooks'
 import { FirebaseApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { useEffect, useState } from 'react'
@@ -9,35 +10,41 @@ import {
 	useHref,
 	useNavigate
 } from 'react-router'
+import { BucketList } from './components/Bucket/list'
 import { List } from './components/Collection/list'
 import { Login } from './components/Login'
+import { RecordView } from './components/Record/view'
 import { Private, SignFlow } from './components/Routes'
 import { Splash } from './components/Splash'
+import { BucketsProvider } from './context/bucket'
 import { CollectionProvider } from './context/collection'
 import { CollectionsProvider } from './context/collectionsList'
-import './index.css'
 import { ParamsProvider } from './context/params'
-import { RecordView } from './components/Record/view'
 import { RecordProvider } from './context/record'
-import { BucketsProvider } from './context/bucket'
-import { BucketList } from './components/Bucket/list'
+import './index.css'
 
 const Routing = ({ app }: { app: FirebaseApp }) => {
 	const auth = getAuth(app)
 	const navigate = useNavigate()
-
-	const [isAuthenticated, setIsAuthenticated] = useState(true)
+	const [loading, toggle] = useToggle(true)
+	const [isAuthenticated, setIsAuthenticated] = useState(!auth)
 
 	useEffect(() => {
-		// if (!auth) return
-		// const unsub = auth.onAuthStateChanged(user => setIsAuthenticated(!!user))
-		// return unsub
+		if (!auth) return
+		const unsub = auth.onAuthStateChanged(user => {
+			setIsAuthenticated(!!user)
+			if (loading) toggle(false)
+		})
+		return unsub
 	}, [])
 
 	return (
 		<NextUIProvider id='next-root' navigate={navigate} useHref={useHref}>
 			<Routes>
-				<Route index element={<Splash loading={!isAuthenticated} />} />
+				<Route
+					index
+					element={<Splash loading={loading} authenticated={isAuthenticated} />}
+				/>
 				<Route
 					path='login'
 					element={<SignFlow authenticated={isAuthenticated} />}>
