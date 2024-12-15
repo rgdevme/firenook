d28bb97d58810434ffc34313ea5439831591a815
Rafael González <reggon81@gmail.com>
2024-12-15 14:42:41 +0100

    fix: made splash screeen redirect correctly

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,15 +1,27 @@
-import { Navigate } from 'react-router'
-import { useAppConfig } from '../firebase'
+import { useEffect } from 'react'
+import { useNavigate, useSearchParams } from 'react-router'
+import { useAppConfig } from '../context'
 import { Path } from '../routes'
 
 export const Splash = () => {
 	const { loading, user } = useAppConfig()
+	const [search] = useSearchParams()
+	const nav = useNavigate()
 
-	return loading ? (
-		<div>Loading</div>
-	) : user ? (
-		<Navigate to={Path.DASHBOARD} replace />
-	) : (
-		<Navigate to={Path.LOGIN} replace />
-	)
+	console.log({ user, loading })
+
+	useEffect(() => {
+		if (loading) return
+		console.log({ user, loading })
+		const redir = search.get('redir')
+		nav(
+			user
+				? Path.DASHBOARD
+				: redir
+				? `${Path.LOGIN}/?redir=${redir}`
+				: Path.LOGIN
+		)
+	}, [loading])
+
+	return <div>Loading</div>
 }
