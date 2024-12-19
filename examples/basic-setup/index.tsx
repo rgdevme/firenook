5647f5faafede2b87384da52b1a58e4c8eec5ecb
Rafael González <reggon81@gmail.com>
2024-12-19 12:38:33 +0100

    fix(example): update the firebase initialization method, add bucket plugin, add styles, add vite.config for react

--- examples/basic-setup/index.tsx
+++ examples/basic-setup/index.tsx
@@ -1,26 +1,25 @@
+import { BucketsPlugin } from '@firenook/bucket'
+import { Firenook, initializeFirebase } from '@firenook/core'
 import { createRoot } from 'react-dom/client'
-import { Firenook } from '@firenook/core'
-import { BucketsPlugin } from '@firenook/bucket/src'
-import { StoresPlugin } from '@firenook/store/src'
 
 import logo from './assets/logo.png'
 
-const config = {
-	apiKey: 'AIzaSyCHJdqYGjsXWlciLt21lN5i8qheX6tBqoQ',
-	authDomain: 'rgdevme.firebaseapp.com',
-	projectId: 'rgdevme',
-	storageBucket: 'rgdevme.firebasestorage.app',
-	messagingSenderId: '615649263196',
-	appId: '1:615649263196:web:26c5e77cf3c4f56de1f099',
-	measurementId: 'G-7P0LGV13FP'
-}
+import '@firenook/core/index.css'
+
+const config = initializeFirebase(
+	{
+		apiKey: 'AIzaSyCHJdqYGjsXWlciLt21lN5i8qheX6tBqoQ',
+		authDomain: 'rgdevme.firebaseapp.com',
+		projectId: 'rgdevme',
+		storageBucket: 'rgdevme.firebasestorage.app',
+		messagingSenderId: '615649263196',
+		appId: '1:615649263196:web:26c5e77cf3c4f56de1f099',
+		measurementId: 'G-7P0LGV13FP'
+	},
+	true
+)
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
-	<Firenook
-		config={config}
-		useEmulator={true}
-		logo={logo}
-		plugins={[StoresPlugin, BucketsPlugin]}
-	/>
+	<Firenook {...config} logo={logo} plugins={[BucketsPlugin(config)]} />
 )
