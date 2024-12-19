4b8f0127947079f1f9de7b8105247cffa859b5b5
Rafael González <reggon81@gmail.com>
2024-12-19 12:55:35 +0100

    fix(example): remove firebase config credentials

--- examples/basic-setup/index.tsx
+++ examples/basic-setup/index.tsx
@@ -1,23 +1,12 @@
 import { BucketsPlugin } from '@firenook/bucket'
 import { Firenook, initializeFirebase } from '@firenook/core'
 import { createRoot } from 'react-dom/client'
-
-import logo from './assets/logo.png'
+import firebaseConfig from './firebase.config.json'
 
 import '@firenook/core/index.css'
+import logo from '../../assets/logo.png'
 
-const config = initializeFirebase(
-	{
-		apiKey: 'AIzaSyCHJdqYGjsXWlciLt21lN5i8qheX6tBqoQ',
-		authDomain: 'rgdevme.firebaseapp.com',
-		projectId: 'rgdevme',
-		storageBucket: 'rgdevme.firebasestorage.app',
-		messagingSenderId: '615649263196',
-		appId: '1:615649263196:web:26c5e77cf3c4f56de1f099',
-		measurementId: 'G-7P0LGV13FP'
-	},
-	true
-)
+const config = initializeFirebase(firebaseConfig, true)
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
