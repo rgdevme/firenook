3d67674fad1c3eba698a7f83b14fda601ca37463
Rafael González <reggon81@gmail.com>
2025-01-16 01:39:36 +0100

    feat(core): extract initialization logic from context, for better and more readable code

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -2,30 +2,27 @@ import { MantineProvider } from '@mantine/core'
 import { FirebaseOptions } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { Fireborm, FirebormSettings } from 'fireborm'
-import { Provider, useAtomValue } from 'jotai'
+import { Provider } from 'jotai'
 import { Suspense, useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { Loading } from './componets/layout/loading'
 import { PrivateLayout } from './componets/layout/private'
 import { PublicLayout } from './componets/layout/public'
+import { getAppState, useAppState } from './context'
 import {
-	authed,
-	getSettingsStore,
-	orm,
-	pluginRoutes,
-	pluginsFunctions,
-	plugins as ps,
-	state
-} from './context'
+	initializeAppState,
+	MenuItemElement,
+	RouteElement
+} from './context/app'
 import { ColorName, mantineColors } from './styles/colors'
 import './styles/index.css'
 import { resolvedTailwindTheme as theme } from './styles/resolvedTailwindConfig'
 import { FirenookPluginFunction } from './types'
+import { state } from './context/utils'
 
 export * from './componets'
+export * from './context'
 export * from './types'
-export { getSettingsStore }
-export const getFireborm = () => orm.get()
 
 interface InitFirebormProps {
 	config: FirebaseOptions
@@ -33,14 +30,37 @@ interface InitFirebormProps {
 	plugins?: FirenookPluginFunction[]
 }
 
+initializeAppState()
+
 const initializeContext = async ({
 	config,
 	settings = {},
 	plugins = []
 }: InitFirebormProps) => {
-	orm.set(new Fireborm(config, settings))
-	pluginsFunctions.set(plugins)
-	console.log('setting plugins functions')
+	const results = await Promise.all(plugins.map(f => f()))
+	const routes: RouteElement[] = []
+	const menuItems: MenuItemElement[] = []
+
+	results.forEach(({ name, routes: r = {}, menu: m = {} }) => {
+		routes.push(
+			...Object.entries(r).map(([path, element]) => ({
+				key: `${name}-${path}`,
+				path,
+				element
+			}))
+		)
+		menuItems.push(
+			...Object.entries(m).map(([id, { element, priority = 10 }]) => ({
+				key: `${name}-${id}`,
+				element,
+				priority
+			}))
+		)
+	})
+
+	getAppState('fireborm').set(new Fireborm(config, settings))
+	getAppState('routes').set(routes)
+	getAppState('menuItems').set(menuItems)
 }
 
 export interface FirenookProps extends InitFirebormProps {
@@ -85,15 +105,16 @@ export const Firenook = ({
 }
 
 const App = () => {
-	useAtomValue(ps)
-	const routes = useAtomValue(pluginRoutes.atom)
-
-	const fireborm = useAtomValue(orm.atom)
-	const auth = getAuth(fireborm?.app)
+	// useAtomValue(ps)
+	const [routes] = useAppState<RouteElement[]>('routes')
+	const [fireborm] = useAppState<Fireborm>('fireborm')
+	const auth = !fireborm ? null : getAuth(fireborm?.app)
 
 	useEffect(() => {
 		if (!auth) return
-		const unsub = auth.onAuthStateChanged(user => authed.set(!!user))
+		const unsub = auth.onAuthStateChanged(user =>
+			getAppState('authed').set(!!user)
+		)
 		return unsub
 	}, [auth])
 
