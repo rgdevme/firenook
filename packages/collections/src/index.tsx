eb92c3555049eb752987eecb729e42122ede91f2
Rafael González <reggon81@gmail.com>
2025-01-19 03:54:21 +0100

    fix(collections): register some property schemas and update some views

--- packages/collections/src/index.tsx
+++ packages/collections/src/index.tsx
@@ -11,13 +11,44 @@ import { Document } from './views/document'
 import { CreateDocument } from './views/document.create'
 import { CollectionMenu } from './views/menu'
 
-export const collectionsAtom = registerAppState<CollectionData[]>(
-	'collections',
-	[]
-)
+import '@mantine/dates/styles.css' //if using mantine date picker features
+import 'mantine-react-table/styles.css' //import MRT styles
+import { NumberField, StringField } from './components/property'
+import { registerPropertySchema } from './components/property/context'
 
 export const Collections: FirenookPluginFunction = () => {
 	const store = getAppState<FirebormStore<{}>>('settingsStore').get()
+
+	registerAppState<CollectionData[]>('collections', [], true)
+	registerAppState('property_schema', new Map())
+
+	registerPropertySchema('string', {
+		name: '',
+		label: '',
+		description: '',
+		defaultValue: '',
+		side: 'left',
+		filterable: true,
+		isArray: false,
+		nullable: false,
+		show: true,
+		sortable: true,
+		element: StringField
+	})
+
+	registerPropertySchema('number', {
+		name: '',
+		label: '',
+		description: '',
+		defaultValue: 0,
+		side: 'left',
+		filterable: true,
+		isArray: false,
+		nullable: false,
+		show: true,
+		sortable: true,
+		element: NumberField
+	})
 
 	return {
 		name: 'collections',
@@ -25,11 +56,11 @@ export const Collections: FirenookPluginFunction = () => {
 			['col/new']: () => <CreateCollection />,
 			['col/:col_id/edit']: () => <CreateCollection edit />,
 			['col/:col_id']: () => <Collection store={store} />,
-			['col/:col_id/new']: () => <CreateDocument store={store} />,
-			['col/:col_id/doc/:doc_id']: () => <Document store={store} />,
+			['col/:col_id/new']: () => <CreateDocument />,
+			['col/:col_id/doc/:doc_id']: () => <CreateDocument edit />,
 			['col/:col_id/sub/:sub_id']: () => <Collection store={store} />,
 			['col/:col_id/sub/:sub_id/edit']: () => <CreateCollection edit />,
-			['col/:col_id/sub/:sub_id/new']: () => <CreateDocument store={store} />,
+			['col/:col_id/sub/:sub_id/new']: () => <CreateDocument />,
 			['col/:col_id/sub/:sub_id/:doc_id']: () => <Document store={store} />
 		},
 		menu: {
