3e9a8a562f8c8c2d086f4120a40dfde73407e4ca
Rafael González <reggon81@gmail.com>
2025-01-23 23:11:54 +0100

    fix(collections): remove unused views

--- packages/collections/src/index.tsx
+++ packages/collections/src/index.tsx
@@ -4,23 +4,22 @@ import {
 	registerAppState
 } from '@firenook/core'
 import { FirebormStore } from 'fireborm'
-import { CollectionData } from './types/collection'
 import { Collection } from './views/collection'
 import { CreateCollection } from './views/collection.create'
-import { Document } from './views/document'
 import { CreateDocument } from './views/document.create'
 import { CollectionMenu } from './views/menu'
 
 import '@mantine/dates/styles.css' //if using mantine date picker features
 import 'mantine-react-table/styles.css' //import MRT styles
-import { NumberField } from './components/property/number'
 import { registerPropertySchema } from './components/property/context'
+import { NumberField } from './components/property/number'
 import { StringField } from './components/property/string'
+import { createContext } from './context/collections'
 
 export const Collections: FirenookPluginFunction = () => {
 	const store = getAppState<FirebormStore<{}>>('settingsStore').get()
 
-	registerAppState<CollectionData[]>('collections', [], true)
+	createContext()
 	registerAppState('property_schema', new Map())
 
 	registerPropertySchema({
@@ -64,16 +63,10 @@ export const Collections: FirenookPluginFunction = () => {
 			['col/:col_id/edit']: () => <CreateCollection edit />,
 			['col/:col_id']: () => <Collection store={store} />,
 			['col/:col_id/new']: () => <CreateDocument />,
-			['col/:col_id/doc/:doc_id']: () => <CreateDocument edit />,
-			['col/:col_id/sub/:sub_id']: () => <Collection store={store} />,
-			['col/:col_id/sub/:sub_id/edit']: () => <CreateCollection edit />,
-			['col/:col_id/sub/:sub_id/new']: () => <CreateDocument />,
-			['col/:col_id/sub/:sub_id/:doc_id']: () => <Document store={store} />
+			['col/:col_id/doc/:doc_id']: () => <CreateDocument edit />
 		},
 		menu: {
-			collections: {
-				element: () => <CollectionMenu />
-			}
+			collections: { element: CollectionMenu }
 		}
 	}
 }
