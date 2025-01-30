9525930a7a04a6d57e3f3ed0d37111a6743f06a1
Rafael González <reggon81@gmail.com>
2025-01-30 03:21:13 +0100

    fix(collections): remove mantine-react-tables as it was introducing dependy conflicts

--- packages/collections/src/views/collection.tsx
+++ packages/collections/src/views/collection.tsx
@@ -2,129 +2,88 @@ import { useField } from '@firenook/core'
 import {
 	ActionIcon,
 	Button,
+	Checkbox,
 	Flex,
 	Menu,
+	Pagination,
 	Popover,
+	Select,
+	Table,
 	Text,
 	TextInput
 } from '@mantine/core'
-import { MantineReactTable, useMantineReactTable } from 'mantine-react-table'
-import { FC, useMemo } from 'react'
+import { FC, ReactNode, useEffect, useMemo, useState } from 'react'
 import { TbDotsVertical, TbFilter, TbPlus, TbSearch } from 'react-icons/tb'
 import { Link, useParams } from 'react-router'
-import {
-	Cell,
-	CellActionCopy,
-	CellActionEdit,
-	CellActionTrash
-} from '../components/cell'
 import { useCollectionStore } from '../context/collections'
-import { useQueryResult } from '../hooks/useQueryresult'
-import { useTableColumns } from '../hooks/useTableColumns'
+import { useQuery } from '../hooks/useQueryresult'
 import { CollectionData, CollectionSchemaProperty } from '../types/collection'
 
 export const Collection: FC = () => {
 	const { col_id } = useParams()
 	const { collection, store } = useCollectionStore()
-	const [query, { onPaginationChange, getInputProps }] = useQueryResult({
-		collection,
-		store
-	})
+	const [
+		{ data, count, pagination },
+		{ getQuery, onChangeCount, onChangePage, onChangeSize, getInputProps }
+	] = useQuery(collection?.schema)
 	const stringFilters = useStringFilters(collection)
+	const [selectedRows, setSelectedRows] = useState<number[]>([])
 
-	const columns = useTableColumns({
-		collection,
-		Cell: ({ row, first, column }) => {
-			return (
-				<Cell
-					column={column}
-					row={row}
-					actions={
-						first && (
-							<>
-								<CellActionCopy value={row.original.id} />
-								<CellActionEdit to={`/col/${col_id}/doc/${row.original.id}`} />
-								<CellActionTrash
-									onClick={() => store?.destroy(row.original.id)}
-								/>
-							</>
-						)
-					}
-				/>
-			)
-		}
-	})
+	useEffect(() => {
+		if (!store) return
 
-	const table = useMantineReactTable({
-		columns,
-		data: query.data,
-		layoutMode: 'grid',
-		initialState: { density: 'xs' },
-		mantinePaperProps: {
-			withBorder: false,
-			shadow: undefined
-		},
-		enableStickyHeader: true,
-		enableSelectAll: true,
-		enableRowSelection: true,
-		enableGlobalFilter: false,
-		enableTopToolbar: false,
-		enableFilters: false,
-		manualFiltering: true,
-		manualPagination: true,
-		manualSorting: true,
-		mantinePaginationProps: {
-			controls: false
-		},
-		renderEmptyRowsFallback: () => null,
-		onPaginationChange,
-		getRowId: row => row.id,
-		rowCount: query.count,
-		state: {
-			isLoading: false,
-			showProgressBars: false,
-			showAlertBanner: false,
-			pagination: query.pagination
-		}
-	})
+		const constrains = getQuery()
+
+		store.count(constrains.where).then(count => onChangeCount(count))
+		store.subscribeMany(constrains)
+	}, [store, getQuery])
 
 	return !collection ? (
 		'loading'
 	) : (
-		<div>
-			<Flex gap='md'>
+		<Flex direction='column' gap='xs' h='100%'>
+			<Flex gap='md' flex='0 0 0'>
 				<TextInput
 					name='search'
 					placeholder='Search'
 					flex='1 1 auto'
 					leftSection={<TbSearch />}
+					rightSectionProps={{
+						style: {
+							justifyContent: 'flex-end',
+							width: 'min-content',
+							paddingRight: 2
+						}
+					}}
+					rightSection={
+						<Popover position='bottom-end'>
+							<Popover.Target>
+								<Button size='xs' variant='subtle' leftSection={<TbFilter />}>
+									Filters
+								</Button>
+							</Popover.Target>
+							<Popover.Dropdown p={4}>
+								<Flex direction='column' gap='xs'>
+									<Text size='xs' c='dimmed' fw={500} px='xs' py={4}>
+										Filtering options
+									</Text>
+									{stringFilters.filter && (
+										<stringFilters.filter
+											fields={stringFilters.properties}
+											keyname='filters'
+											label=''
+											type='string'
+											isDirty={false}
+											isSubmitting={false}
+											{...getInputProps('searchBy')}
+										/>
+									)}
+								</Flex>
+							</Popover.Dropdown>
+						</Popover>
+					}
 					{...getInputProps('search')}
 				/>
-				<Popover position='bottom-end'>
-					<Popover.Target>
-						<ActionIcon size='input-sm' variant='subtle'>
-							<TbFilter />
-						</ActionIcon>
-					</Popover.Target>
-					<Popover.Dropdown p={4}>
-						<Flex direction='column' gap='xs'>
-							<Text size='xs' c='dimmed' fw={500} px='xs' py={4}>
-								Filtering options
-							</Text>
-							{stringFilters.filter && (
-								<stringFilters.filter
-									fields={stringFilters.properties}
-									keyname='filters'
-									label=''
-									type='string'
-									isDirty={false}
-									isSubmitting={false}
-									{...getInputProps('searchBy')}
-								/>
-							)}
-						</Flex>
-					</Popover.Dropdown>
-				</Popover>
 				<Button
 					variant='light'
 					leftSection={<TbPlus />}
@@ -149,8 +108,71 @@ export const Collection: FC = () => {
 				</Menu>
 			</Flex>
 
-			<MantineReactTable table={table} />
-		</div>
+			<Table
+				flex='1 0 auto'
+				striped
+				highlightOnHover
+				horizontalSpacing='sm'
+				verticalSpacing='sm'
+				stickyHeader
+				stickyHeaderOffset={0}
+				data={{
+					head: [
+						<Checkbox
+							aria-label='Select all'
+							checked={selectedRows.length === data.length && data.length > 0}
+							onChange={event =>
+								setSelectedRows(
+									event.currentTarget.checked ? data.map(x => x.id) : []
+								)
+							}
+						/>,
+						...collection.schema.map(s => s.label)
+					],
+					body: data.map(x => [
+						<Checkbox
+							aria-label='Select row'
+							checked={selectedRows.includes(x.id)}
+							onChange={event =>
+								setSelectedRows(
+									event.currentTarget.checked
+										? [...selectedRows, x.id]
+										: selectedRows.filter(id => id !== x.id)
+								)
+							}
+						/>,
+						...collection.schema.map(s => x[s.keyname] as ReactNode)
+					])
+				}}
+			/>
+			<Flex
+				flex='0 0 0'
+				direction='row'
+				wrap='nowrap'
+				gap='xs'
+				align='center'
+				justify='flex-end'>
+				<Select
+					value={pagination.size.toString()}
+					data={[
+						{ label: '1', value: '1' },
+						{ label: '2', value: '2' },
+						{ label: '5', value: '5' },
+						{ label: '10', value: '10' },
+						{ label: '20', value: '20' },
+						{ label: '50', value: '50' },
+						{ label: '100', value: '100' }
+					]}
+					onChange={onChangeSize}
+				/>
+				<Pagination
+					withPages={false}
+					total={Math.ceil(count / pagination.size)}
+					value={pagination.index}
+					onChange={onChangePage}
+				/>
+			</Flex>
+		</Flex>
 	)
 }
 
