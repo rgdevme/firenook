df2a970527c42cd1586f389b1e361e433f511e70
Rafael González <reggon81@gmail.com>
2025-01-30 22:24:52 +0100

    chore(repo): remove dead files

--- src/hooks/useToggleAtom.ts
+++
@@ -1,9 +0,0 @@
-import { PrimitiveAtom, useSetAtom } from 'jotai'
-
-export const useToggleAtom = (atom: PrimitiveAtom<boolean>) => {
-	const setAtom = useSetAtom(atom)
-	return (value?: boolean) => {
-		if (value !== undefined) setAtom(value)
-		else setAtom(p => !p)
-	}
-}
--- src/routes.ts
+++
@@ -1,8 +0,0 @@
-export enum Path {
-	LOGIN = '/login',
-	DASHBOARD = '/dashboard',
-	BUCKET = '/b/:bid',
-	COLLECTION = '/c/:cid',
-	RECORD = '/c/:cid/r/:rid',
-	SUBCOLLECTION = '/c/:cid/r/:rid/s/:sid'
-}
